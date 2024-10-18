65afeae32e5fde374b914fad339529ca42dd8f3d
EDUARDO SANTOS DA SILVA <eduardo.ssilva36@senacsp.edu.br>
2024-10-17 22:17:05 -0300

    Commit

--- funcoes.php
+++ funcoes.php
@@ -52,6 +52,76 @@
         return $resultado;
     }
 
+    function fatorial($num1) {
+        $num1;
+        $res = 1;
+
+        $num1 = "$num1";
+
+        $i = 1;
+        while($num1 > $i){
+            $res = $res * $num1;
+            $num1 = $num1 - 1;
+
+        }
+        return $res;
+
+    }
+
+    function parImpar($num1){
+        $total = "";
+
+        if($num1 % 2 == 0){
+            $total = "Par";
+
+        }else{
+            $total = "Impar";
+        }
+        return $total;
+    }
+
+    function ordemCrescente($num1, $num2) {
+        if($num1 > $num2) {
+            return "O primeiro número é maior";
+        }else{
+            return "O segundo número é maior";
+        }
+
+
+    }
+
+    function primo($num1){
+        if( $num1 == 2 || $num1 == 3 || $num1 == 5){
+            return "O número digitado é primo";
+        }else if($num1 % 2 == 0 || $num1 % 3 == 0 || $num1 % 5 == 0){
+            return "O numero digitado é primo";
+        }else{
+            return "O número digitado não é primo";
+        }
+    }
+
+    function media($num1, $num2, $num3) {
+        $media = 0;
+        $media = ( $num1 + $num2 + $num3 ) / 3;
+        return $media;
+
+    }
+
+    function potencia($num1, $num2) {
+        $pot = "";
+
+        pow.($num1 ** $num2);
+
+        return $pot;
+
+    }
+        
+
+    
+    
+
+    
+
 
 
 
--- index.php
+++ index.php
@@ -17,23 +17,32 @@
     <label>Segundo Numero: </label>
     <input type="text" name="num2" id="num2"/><br><br>
 
+    
+
     <button>Calcular
         <?php
             $num1 = $_POST['num1']; //Coletar os dados
             $num2 = $_POST['num2'];
+        
         ?>
     </button><br><br>
 
         <textarea rows="10" cols="40" readonly fixed>
             <?php
         
-            echo "\nA soma de ".$num1. " com " .$num2. " é: ".somar( $num1,$num2 );
-            echo "\nA subtração de ".$num1. " com " .$num2. " é: ".subtrair( $num1,$num2 );
-            echo "\nA multiplicação de ".$num1. " com " .$num2. " é: ".multiplicar( $num1,$num2 );
-            echo "\nA divisão de ".$num1. " com " .$num2. " é: ".dividir( $num1,$num2 );
-            echo "\nA sequencia de números entre ".$num1." e ".$num2." é: ".mostrarNumeros($num1,$num2); 
-            echo "\nO ".$num1." e ".$num2." é:\n ".verificarNumero($num1, $num2 );
-            echo "\nA tabuada do ".$num1." é:\n ".tabuada($num1);
+            //echo "\nA soma de ".$num1. " com " .$num2. " é: ".somar( $num1,$num2 );
+            //echo "\nA subtração de ".$num1. " com " .$num2. " é: ".subtrair( $num1,$num2 );
+            //echo "\nA multiplicação de ".$num1. " com " .$num2. " é: ".multiplicar( $num1,$num2 );
+            //echo "\nA divisão de ".$num1. " com " .$num2. " é: ".dividir( $num1,$num2 );
+            //echo "\nA sequencia de números entre ".$num1." e ".$num2." é: ".mostrarNumeros($num1,$num2); 
+            //echo "\nO ".$num1." e ".$num2." é:\n ".verificarNumero($num1, $num2 );
+            //echo "\nA tabuada do ".$num1." é:\n ".tabuada($num1);
+            //echo "\nO fatorial do ".$num1. " é:\n ".fatorial($num1);
+            //echo "\nO resultado de ".$num1." é: ".parImpar($num1);
+            //echo ordemCrescente($num1, $num2);
+            //echo primo($num1);
+            //echo media($num1, $num2, $num3);
+            echo potencia($num1, $num2);
             ?>
         </textarea>
 
